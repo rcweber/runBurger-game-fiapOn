5d565705e04706fdbcf43c28fa498b2f155d535d
rcweber <rcorazzaweber@gmail.com>
2024-05-20 17:24:02 -0300

    Create the main menu

---
+++ RunBurger/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button ButtonPlay;
+    [SerializeField] private Button ButtonExit;
+    public void PlayGame() {
+
+        SceneManager.LoadScene("FirstFase");
+    }
+    public void QuitGame() {
+
+        Application.Quit();
+    }
+}
